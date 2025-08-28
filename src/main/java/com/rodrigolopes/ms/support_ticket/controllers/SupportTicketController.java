acabffbb6479b4c46c5ea4ce00a731ca97c01218
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-08-28 20:39:00 -0300

    feat: create delete ticket support endpoint

--- src/main/java/com/rodrigolopes/ms/support_ticket/controllers/SupportTicketController.java
+++ src/main/java/com/rodrigolopes/ms/support_ticket/controllers/SupportTicketController.java
@@ -16,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -54,6 +55,12 @@ public class SupportTicketController {
     public ResponseEntity<ResponseTicketDTO> update(@PathVariable String id, @Valid @RequestBody RequestTicketDTO data) {
         var response = this.supportTicketService.update(UUID.fromString(id), data);
         return ResponseEntity.ok().body(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        this.supportTicketService.delete(UUID.fromString(id));
+        return ResponseEntity.noContent().build();
     }
 
 }
