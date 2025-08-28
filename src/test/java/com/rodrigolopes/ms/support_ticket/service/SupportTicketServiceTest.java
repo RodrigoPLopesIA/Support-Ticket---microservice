327ade2e3d2ea426d91c8cdce31cd831697669c0
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-08-28 16:55:21 -0300

    test: enhance SupportTicketServiceTest with ticket creation logic

--- src/test/java/com/rodrigolopes/ms/support_ticket/service/SupportTicketServiceTest.java
+++ src/test/java/com/rodrigolopes/ms/support_ticket/service/SupportTicketServiceTest.java
@@ -1,5 +1,6 @@
 package com.rodrigolopes.ms.support_ticket.service;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -7,14 +8,16 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import com.rodrigolopes.ms.support_ticket.dto.RequestTicketDTO;
 import com.rodrigolopes.ms.support_ticket.entities.SupportTicket;
+import com.rodrigolopes.ms.support_ticket.mapper.TicketMapper;
 import com.rodrigolopes.ms.support_ticket.repositories.TicketSupportRepository;
 import com.rodrigolopes.ms.support_ticket.services.SupportTicketService;
 
 @ExtendWith(MockitoExtension.class)
 public class SupportTicketServiceTest {
-    
 
     @InjectMocks
     private SupportTicketService supportTicketService;
@@ -22,11 +25,25 @@ public class SupportTicketServiceTest {
     @Mock
     private TicketSupportRepository ticketSupportRepository;
 
-
+    @Autowired 
+    private TicketMapper ticketMapper;
     @Test
     @DisplayName("Should create a support ticket successfully")
     public void testCreateSupportTicket() {
         
+        SupportTicket supportTicket = SupportTicket.builder()
+                .title("Issue with login")
+                .description("Unable to login with correct credentials")
+                .build();
+        RequestTicketDTO requestDto = ticketMapper.toRequestDto(supportTicket);
+        Mockito.when(ticketSupportRepository.save(supportTicket)).thenReturn(supportTicket);
 
+
+        var result = supportTicketService.createTicket(requestDto);
+
+
+        Assertions.assertThat(result).isNotNull();
+
+        
     }
 }
