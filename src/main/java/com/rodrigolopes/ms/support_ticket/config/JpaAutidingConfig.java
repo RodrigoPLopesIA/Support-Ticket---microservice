2f367db5c1058c7785c7cada1418d9eb16eb3473
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-08-28 20:05:37 -0300

    refactor: add configuration annotation to enable jpa auditing

--- src/main/java/com/rodrigolopes/ms/support_ticket/config/JpaAutidingConfig.java
+++ src/main/java/com/rodrigolopes/ms/support_ticket/config/JpaAutidingConfig.java
@@ -1,7 +1,9 @@
 package com.rodrigolopes.ms.support_ticket.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@Configuration
 @EnableJpaAuditing
 public class JpaAutidingConfig {
     
