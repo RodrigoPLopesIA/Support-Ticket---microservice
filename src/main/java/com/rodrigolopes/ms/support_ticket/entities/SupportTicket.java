c270fcc2216260558e24e5b78ec0459fd6c4f5de
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-08-28 16:55:26 -0300

    feat: add Lombok @Builder annotation to SupportTicket entity

--- src/main/java/com/rodrigolopes/ms/support_ticket/entities/SupportTicket.java
+++ src/main/java/com/rodrigolopes/ms/support_ticket/entities/SupportTicket.java
@@ -20,11 +20,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Builder
 @Entity
 @Getter
 @Setter
