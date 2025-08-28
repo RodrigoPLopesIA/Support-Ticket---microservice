94b0fe3405be4be4d1044ca4cc46d7cdd81b3b4b
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-08-28 20:15:31 -0300

    refactor: import correct method argument not valid exception

--- src/main/java/com/rodrigolopes/ms/support_ticket/exceptions/ControllerErrorHandler.java
+++ src/main/java/com/rodrigolopes/ms/support_ticket/exceptions/ControllerErrorHandler.java
@@ -1,14 +1,13 @@
 package com.rodrigolopes.ms.support_ticket.exceptions;
 
-import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.support.MethodArgumentNotValidException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -36,13 +35,13 @@ public class ControllerErrorHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponseDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex,
-            HttpServletRequest request, BindingResult result) {
+            HttpServletRequest request) {
+
         Map<String, String> errors = new HashMap<>();
-        for (FieldError error : result.getFieldErrors()) {
-            errors.put(error.getField(), error.getDefaultMessage());
-        }
+        ex.getBindingResult().getFieldErrors()
+                .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(new ErrorResponseDTO(request.getRequestURI(), ex.getMessage(), HttpStatus.BAD_REQUEST.value(),
+                .body(new ErrorResponseDTO(request.getRequestURI(), "Invalid arguments!", HttpStatus.BAD_REQUEST.value(),
                         errors));
     }
 }
