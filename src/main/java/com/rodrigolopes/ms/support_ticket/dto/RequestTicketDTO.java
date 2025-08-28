f0f8c45e36cc3f3c79919f535a7c7c7a7ee4b4d5
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-08-28 20:15:52 -0300

    feat: implement error message in not blank annotation

--- src/main/java/com/rodrigolopes/ms/support_ticket/dto/RequestTicketDTO.java
+++ src/main/java/com/rodrigolopes/ms/support_ticket/dto/RequestTicketDTO.java
@@ -2,6 +2,7 @@ package com.rodrigolopes.ms.support_ticket.dto;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record RequestTicketDTO(@NotBlank String title, @NotBlank String description, String status) {
+public record RequestTicketDTO(@NotBlank(message = "Title must not be empty") String title,
+        @NotBlank(message = "Description must not be empty") String description, String status) {
 
 }
