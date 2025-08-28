f76a5f04c12183d2035e97be5f5703b7f935c487
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-08-28 20:06:04 -0300

    refactor: remove unused imports

--- src/main/java/com/rodrigolopes/ms/support_ticket/controllers/SupportTicketController.java
+++ src/main/java/com/rodrigolopes/ms/support_ticket/controllers/SupportTicketController.java
@@ -18,7 +18,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
