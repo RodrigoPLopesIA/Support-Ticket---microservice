abc3fbacaf90d4b8e78e968d6b6735291715aa5a
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-08-28 18:45:54 -0300

    test: update delete support ticket test to use existsById and deleteById methods

--- src/test/java/com/rodrigolopes/ms/support_ticket/service/SupportTicketServiceTest.java
+++ src/test/java/com/rodrigolopes/ms/support_ticket/service/SupportTicketServiceTest.java
@@ -148,9 +148,11 @@ public class SupportTicketServiceTest {
     @DisplayName("Should delete a support ticket successfully")
     public void testDeleteSupportTicket() {
         var id = UUID.randomUUID();
-        Mockito.when(ticketSupportRepository.findById(id)).thenReturn(Optional.of(supportTicket));
-        supportTicketService.delete(id);
-        Mockito.verify(ticketSupportRepository, Mockito.times(1)).delete(supportTicket);
+        Mockito.when(ticketSupportRepository.existsById(id)).thenReturn(true);
         
+        supportTicketService.delete(id);
+        
+        Mockito.verify(ticketSupportRepository, Mockito.times(1)).deleteById(id);
+
     }
 }
