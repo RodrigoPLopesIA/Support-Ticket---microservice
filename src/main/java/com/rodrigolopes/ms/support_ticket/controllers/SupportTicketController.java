f04ec4b4cdd2c8a13c49c60e497ab6cdc2dd334c
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-08-28 19:39:10 -0300

    feat: add get endpoint to SupportTicketController for retrieving ticket by ID

--- src/main/java/com/rodrigolopes/ms/support_ticket/controllers/SupportTicketController.java
+++ src/main/java/com/rodrigolopes/ms/support_ticket/controllers/SupportTicketController.java
@@ -6,11 +6,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.rodrigolopes.ms.support_ticket.dto.ResponseTicketDTO;
 import com.rodrigolopes.ms.support_ticket.services.SupportTicketService;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/tickets")
@@ -24,6 +28,11 @@ public class SupportTicketController {
         var data = this.supportTicketService.getAll(pageable);
         ;
         return ResponseEntity.ok().body(data);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseTicketDTO> getMethodName(@PathVariable String id) {
+        return ResponseEntity.ok().body(this.supportTicketService.getById(UUID.fromString(id)));
     }
 
 }
