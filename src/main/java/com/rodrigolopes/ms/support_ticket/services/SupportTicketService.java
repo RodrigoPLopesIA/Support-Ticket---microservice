0862d1602e0fd7352fe4f31924a957c19d9fda14
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-08-28 16:37:01 -0300

    feat: implement SupportTicketService with createTicket method

---
+++ src/main/java/com/rodrigolopes/ms/support_ticket/services/SupportTicketService.java
@@ -0,0 +1,20 @@
+package com.rodrigolopes.ms.support_ticket.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rodrigolopes.ms.support_ticket.repositories.TicketSupportRepository;
+
+@Service
+public class SupportTicketService {
+    
+
+    @Autowired
+    private TicketSupportRepository ticketSupportRepository;
+
+
+
+    public ResponseTicketDTO createTicket(RequestTicketDTO requestTicketDTO) {
+        
+    }
+}
