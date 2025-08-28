340fe3b1c6ad34965250816349d13481824e1ec0
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-08-28 16:32:43 -0300

    feat: create TicketSupportRepository interface for SupportTicket entity

---
+++ src/main/java/com/rodrigolopes/ms/support_ticket/repositories/TicketSupportRepository.java
@@ -0,0 +1,11 @@
+package com.rodrigolopes.ms.support_ticket.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rodrigolopes.ms.support_ticket.entities.SupportTicket;
+
+public interface TicketSupportRepository extends JpaRepository<SupportTicket, UUID> {
+    
+}
