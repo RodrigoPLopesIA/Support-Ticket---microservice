89d7cb3f060dee58cbe7055e42e0a471d73daf96
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-08-28 19:18:00 -0300

    feat: update SupportTicketController to return paginated response of tickets

--- src/main/java/com/rodrigolopes/ms/support_ticket/controllers/SupportTicketController.java
+++ src/main/java/com/rodrigolopes/ms/support_ticket/controllers/SupportTicketController.java
@@ -2,17 +2,28 @@ package com.rodrigolopes.ms.support_ticket.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rodrigolopes.ms.support_ticket.dto.ResponseTicketDTO;
+import com.rodrigolopes.ms.support_ticket.services.SupportTicketService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-
 
 @RestController
 @RequestMapping("/tickets")
 public class SupportTicketController {
 
+    @Autowired
+    private SupportTicketService supportTicketService;
+
     @GetMapping()
-    public ResponseEntity<String> index() {
-        return ResponseEntity.ok().body("hello world");
+    public ResponseEntity<Page<ResponseTicketDTO>> index(Pageable pageable) {
+        var data = this.supportTicketService.getAll(pageable);
+        ;
+        return ResponseEntity.ok().body(data);
     }
 
 }
