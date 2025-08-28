864bec970bf02815847fce561c1a11ab938bc976
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-08-28 18:39:54 -0300

    feat: implement update method for SupportTicketService to handle ticket updates with title uniqueness check

--- src/main/java/com/rodrigolopes/ms/support_ticket/services/SupportTicketService.java
+++ src/main/java/com/rodrigolopes/ms/support_ticket/services/SupportTicketService.java
@@ -4,12 +4,16 @@ import com.fasterxml.jackson.databind.DatabindException;
 import com.rodrigolopes.ms.support_ticket.enums.TicketStatus;
 import com.rodrigolopes.ms.support_ticket.mapper.TicketMapper;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rodrigolopes.ms.support_ticket.dto.RequestTicketDTO;
 import com.rodrigolopes.ms.support_ticket.dto.ResponseTicketDTO;
 import com.rodrigolopes.ms.support_ticket.repositories.TicketSupportRepository;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class SupportTicketService {
@@ -30,5 +34,22 @@ public class SupportTicketService {
         var createdTicket = ticketSupportRepository.save(supportTicket);
         return ticketMapper.toResponseDto(createdTicket);
 
+    }
+
+    public ResponseTicketDTO update(UUID id, RequestTicketDTO requestDto) {
+        var existingTicket = ticketSupportRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Ticket not found with id: " + id));
+
+        if (ticketSupportRepository.existsByTitleAndIdNot(requestDto.title(), id)) {
+            throw new IllegalArgumentException("A ticket with this title already exists in another record.");
+        }
+        
+        existingTicket.setTitle(requestDto.title());
+        existingTicket.setDescription(requestDto.description());
+        existingTicket.setStatus(TicketStatus.valueOf(requestDto.status()));
+
+        var updatedTicket = ticketSupportRepository.save(existingTicket);
+
+        return ticketMapper.toResponseDto(updatedTicket);
     }
 }
